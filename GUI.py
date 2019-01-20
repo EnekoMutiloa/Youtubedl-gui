e067b12a680d0c646d993548f1346cd85a5ec0f1
useTab <enekomutiloa@gmail.com>
2019-01-20 21:12:28 +0100

    Primeras clases y codigos para obtener una interfaz

--- GUI.py
+++ GUI.py
@@ -1,22 +1,25 @@
-from tkinter import Tk, Label, Button, Text, messagebox, Frame
-import TextFile
+from tkinter import *
+from tkinter import messagebox
+from TextFile import TextFile
 import subprocess
 
 
 class MusicDownloadGUI:
     def __init__(self, master, height, width):
-        #f = TextFile()
-        #f.create_text_file()
+        f = TextFile()
+        f_name = 'list.txt'
+        f.create_text_file(f_name)
 
         self.master = master
         master.title("Descargar música")
         master.geometry(str(height)+"x"+str(width))
         master.resizable(0, 0)
 
-        top_frame = Frame(master, width = width, height= height, pady=3).grid(row=0, columnspan=3)
-        Label(top_frame, text='Programa para descargar musica').grid(row=0, columnspan=3)
+        #Label(master, text="Programa para decsargar musica").grid(row=0)
+        #e1 = Entry(master)
+        #e1.grid(row=0, column=1)
 
-        self.download_button = Button(master, text ="Descargar", command = lambda: self.download_from_text("a.txt")).pack()
+        self.download_button = Button(master, text ="Descargar", command = lambda: self.download_from_text(f)).pack()
 
         #T = Text(root, height=2, width=30)
         #T.pack()
@@ -24,15 +27,20 @@ class MusicDownloadGUI:
         #self.label = Label(master, text="This is our first GUI!")
         #self.label.pack()
 
-        self.close_button = Button(master, text = "Close", command = master.quit).pack()
+        self.close_button = Button(master, text = "Close", command = self.exit_button(f)).pack()
 
 
     def download_from_text(self, text_file):
 
-        res = subprocess.check_output(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "https://www.youtube.com/watch?v=AXvr66tOERo&frags=pl%2Cwn"]) #"-a", #text_file.file])
         print("Boton pulsado. Descargando")
-        #text_file.close_and_delete_file()
+        res = subprocess.check_output(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-a", text_file.get_name])
+        text_file.close_and_delete_file()
         messagebox.showinfo("Hecho", "Se ha completado la descarga.")
+        return None
+
+    def exit_button(self, text_file):
+        text_file.close_and_delete_file()
+        self.master.quit
         return None
 
 
--- TextFile.py
+++ TextFile.py
@@ -5,14 +5,18 @@ class TextFile:
 
     def __init__(self):
         self.file = None
+        self.name = None
 
-    def create_text_file(self):
-        self.file = open('list.txt', 'w')
+    def create_text_file(self, name):
+        self.name = name
+        self.file = open(self.name, 'w')
 
     def close_and_delete_file(self):
         self.file.close()
-        os.remove(self.file)
+        os.remove(self.name)
 
     def write_in_file(self, url):
         self.file.write(url)
 
+    def get_name(self):
+        return self.name
