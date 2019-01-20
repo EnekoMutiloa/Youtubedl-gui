9e048ca37fffe410d531fbcc9bd1f23a07f94fc9
useTab <enekomutiloa@gmail.com>
2019-01-20 22:54:40 +0100

    Primeras clases y codigos para obtener una interfaz

--- GUI.py
+++ GUI.py
@@ -32,46 +32,43 @@ class MusicDownloadGUI:
     def __init__(self, master, height, width):
         '''Metodo constructor de la clase que genera la interfaz'''
         self.master = master
-        #master.geometry(str(height)+"x"+str(width))
         self.label = None
-        self.entry = None
+        self.entrada_variable = None
         self.download_button = None
-        self.topFrame = None
-        self.entryScrollbar = None
-        self.buttonFrame = None
+        self.top_frame = None
+        self.entry_scrollbar = None
+        self.button_frame = None
 
         f_name = 'list.txt'
-        f = TextFile(f_name)
-        f.create_text_file()
+        self.file = TextFile(f_name)
+        self.file.create_text_file()
 
-        self.createWidgets(f)
-        #T = Text(root, height=2, width=30)
-        #T.pack()
-
-        #self.label = Label(master, text="This is our first GUI!")
-        #self.label.pack()
+        self.createWidgets()
 
 
-    def createWidgets(self, f):
+    def createWidgets(self):
         '''Metodo que crea todos los widgets necesarios para manejar la interfaz. Botones, cuadros de escritura, etc.'''
-        self.topFrame = Frame(relief='raised', bg='gray')
-        self.label = Label(self.master, text="Direcciones:").pack()#(side=LEFT)
-        self.entry = Entry(self.master, relief='sunken').pack()#(side=RIGHT)
-        self.entryScrollbar = Scrollbar(self.entry, orient='vertical', relief='raised')
-        self.topFrame.pack(fill='x', expand=True) #fill='x' quiere decir que aprovecha el espacio horizontal disponible
+        self.top_frame = Frame(relief = 'sunken', bg = 'black')
+        self.label = Label(self.master, text="Escribir a continuación las direcciones de las que se quiere descargar la música. Una línea por url (dirección de internet):").pack()#(side=LEFT)
+        self.entrada_variable = Text(self.master, relief = 'sunken', insertborderwidth = '2.0').pack(fill = 'x', expand = True)
+        #Entry(self.master, relief='sunken').pack()#(side=RIGHT)
+        self.top_frame.pack(fill = 'x', expand = True) #fill='x' quiere decir que aprovecha el espacio horizontal disponible
 
-        self.buttonFrame = Frame(relief='raised', bg='black')
-        self.download_button = Button(self.master, text="Descargar", command = self.download_from_text(f)).pack()
-        self.buttonFrame.pack(fill='x', expand=True)
+        self.button_frame = Frame(relief = 'raised', bg = 'gray')
+        self.download_button = Button(self.master, text="Descargar", command = lambda: self.download_from_text()).pack()
+        self.button_frame.pack(fill = 'x', expand = True)
 
-    def download_from_text(self, text_file):
+    #def download_button_action(self):
+    #    download_from_text()
+
+    def download_from_text(self):
         '''Metodo mediante el cual se realiza la descarga'''
-        print(self.entry.get())
-
-        res = subprocess.check_output(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-a", text_file.get_name])
-        text_file.close_and_delete_file()
+        print(len(self.entrada_variable.get("1.0", END)))
+        self.file.write_in_file(self.entrada_variable.get("1.0", END))
+        res = subprocess.check_output(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-a", self.file.get_name])
+        #https://www.youtube.com/watch?v=AXvr66tOERo&frags=pl%2Cwn
+        self.file.close_and_delete_file()
         messagebox.showinfo("Hecho", "Se ha completado la descarga.")
-        return None
 
 window = Tk()
 window.resizable(0, 0)
