437e9f15269e0e5b01f93dcb2b140fad3df935a1
useTab <enekomutiloa@gmail.com>
2019-01-20 23:40:27 +0100

    cambio de nombre entrada_variable a text_area y adicion de un boton para cerrar el programa

--- GUI.py
+++ GUI.py
@@ -33,7 +33,7 @@ class MusicDownloadGUI:
         '''Metodo constructor de la clase que genera la interfaz'''
         self.master = master
         self.label = None
-        self.entrada_variable = None
+        self.text_area = None
         self.download_button = None
         self.top_frame = None
         self.entry_scrollbar = None
@@ -50,22 +50,22 @@ class MusicDownloadGUI:
         '''Metodo que crea todos los widgets necesarios para manejar la interfaz. Botones, cuadros de escritura, etc.'''
         self.top_frame = Frame(relief = 'sunken', bg = 'black')
         self.label = Label(self.master, text="Escribir a continuación las direcciones de las que se quiere descargar la música. Una línea por url (dirección de internet):").pack()#(side=LEFT)
-        self.entrada_variable = Text(self.master, relief = 'sunken', insertborderwidth = '2.0').pack(fill = 'x', expand = True)
+        self.text_area = Text(self.master, relief = 'sunken', insertborderwidth = '2.0').pack(fill = 'x', expand = True)
         #Entry(self.master, relief='sunken').pack()#(side=RIGHT)
         self.top_frame.pack(fill = 'x', expand = True) #fill='x' quiere decir que aprovecha el espacio horizontal disponible
 
         self.button_frame = Frame(relief = 'raised', bg = 'gray')
-        self.download_button = Button(self.master, text="Descargar", command = lambda: self.download_from_text()).pack()
+        self.download_button = Button(self.master, relief = "raised", text="Descargar", command = lambda: self.download_from_text()).pack(fill = 'x', expand = True)
+        self.exit_button = Button(self.master, relief = "raised", text = "Salir", command = self.master.quit).pack(fill = 'x', expand = True)
         self.button_frame.pack(fill = 'x', expand = True)
 
-    #def download_button_action(self):
-    #    download_from_text()
+
 
     def download_from_text(self):
         '''Metodo mediante el cual se realiza la descarga'''
         ##HAY QUE SEGUIR CON ESTE PUNTO. FALLA EL METODO GET
-        print(len(self.entrada_variable.get("1.0", END)))
-        self.file.write_in_file(self.entrada_variable.get("1.0", END))
+        print(len(self.text_area.get("1.0", END)))
+        self.file.write_in_file(self.text_area.get("1.0", END))
         res = subprocess.check_output(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-a", self.file.get_name])
         #https://www.youtube.com/watch?v=AXvr66tOERo&frags=pl%2Cwn
         self.file.close_and_delete_file()
