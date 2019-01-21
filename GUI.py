0efa2c6f92b4f2b30cad5c02dbf1b337b8045fe2
useTab <enekomutiloa@gmail.com>
2019-01-21 21:40:45 +0100

    Cambios menores

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ try: # para ejecutar en python3.X
 	from tkinter import messagebox
 	import tkinter.font as tkFont
 	import tkinter.ttk as ttk
+	import subprocess
 	import os
 
 except ImportError:  # para jecutar en python 2.7
@@ -12,6 +13,7 @@ except ImportError:  # para jecutar en python 2.7
 	import tkMessageBox as messagebox
 	import tkFont
 	import ttk
+	import subprocess
 	import os
 
 
@@ -45,6 +47,7 @@ class MusicDownloadGUI:
 		self.entry_scrollbar = None
 		self.button_frame = None
 		self.text_scrollbar = None
+		self.exit_button = None
 
 		f_name = 'list.txt'
 		self.file = TextFile(f_name)
@@ -57,12 +60,13 @@ class MusicDownloadGUI:
 		'''Metodo que crea todos los widgets necesarios para manejar la interfaz. Botones, cuadros de escritura, etc.'''
 		self.top_frame = Frame(relief = 'sunken', bg = 'black')
 		self.label = Label(self.master, text="Escribir a continuacion las direcciones de las que se quiere descargar la musica. Una linea por url (direccion de internet):").pack()#(side=LEFT)
-		self.text_area = Text(self.master, relief = 'sunken', insertborderwidth = '2.0').pack(fill = 'x', expand = True)
-		#Entry(self.master, relief='sunken').pack()#(side=RIGHT)
 		self.text_scrollbar = Scrollbar(self.text_area, orient='vertical', relief='raised')
-		self.text_area.config(yscrollcommand=self.text_scrollbar.set)
-		self.text_scrollbar.config(command=self.resultsListbox.yview)
-		self.text_scrollbar.pack(side='right', fill='y', ipady=100)  # cambiando ipady cambia la 'altura' del cuadro donde se escriben las tablas que contienen cierta variable
+		self.text_scrollbar.pack(side = 'right', fill = 'y')  # cambiando ipady cambia la 'altura' del cuadro donde se escriben las tablas que contienen cierta variable
+		self.text_area = Text(self.master, relief = 'sunken', insertborderwidth = '2.0', yscrollcommand = self.text_scrollbar.set)
+		self.text_area.pack(fill = BOTH, expand = True)
+
+		#self.text_scrollbar.config(command = self.text_area.yview)
+
 
 		self.top_frame.pack(fill = 'x', expand = True) #fill='x' quiere decir que aprovecha el espacio horizontal disponible
 
@@ -85,6 +89,6 @@ class MusicDownloadGUI:
 
 window = Tk()
 window.resizable(0, 0)
-window.title("Descargar música")
+window.title("Descargar musica")
 my_gui = MusicDownloadGUI(window, 500, 500)
 window.mainloop()
