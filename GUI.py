78b90daa66f65a02c49724d6e45a409eebf7da52
useTab <enekomutiloa@gmail.com>
2019-01-23 23:11:31 +0100

    Añadido un TODO. Hecha una idea de como se guarda el output para luego procesarlo

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 try: # para ejecutar en python3.X
 	from tkinter import *
 	from tkinter import filedialog
@@ -6,6 +8,7 @@ try: # para ejecutar en python3.X
 	import tkinter.ttk as ttk
 	import subprocess
 	import os
+	import re
 
 except ImportError:  # para jecutar en python 2.7
 	from Tkinter import *
@@ -15,6 +18,7 @@ except ImportError:  # para jecutar en python 2.7
 	import ttk
 	import subprocess
 	import os
+	import re
 
 
 class MusicDownloadGUI:
@@ -24,7 +28,7 @@ class MusicDownloadGUI:
 		'''
 		self.master = master
 		self.master.resizable(0, 0)
-		self.master.title("Descargar musica")
+		self.master.title('Descargar música')
 		self.label = None
 		self.text_area = None
 		self.download_button = None
@@ -33,10 +37,9 @@ class MusicDownloadGUI:
 		self.button_frame = None
 		self.text_scrollbar = None
 		self.exit_button = None
-		self.n_songs = None
 
 		self.f_name = 'list.txt'
-		self.file = open(self.f_name, "w+")
+		self.file = None
 
 		self.initGui(height, width)
 
@@ -47,7 +50,7 @@ class MusicDownloadGUI:
 		:param height: height of the window
 		'''
 
-		self.label = Label(self.master, text = "Escribir a continuacion las direcciones de las que se quiere descargar la musica. Una linea por url (direccion de internet):")
+		self.label = Label(self.master, text='Escribir a continuación las direcciones de las que se quiere descargar la música. Una línea por url (dirección de internet):')
 		self.label.pack()
 
 		'''Create a Frame for the Text and Scrollbar'''
@@ -67,46 +70,69 @@ class MusicDownloadGUI:
 		'''Create a Frame for the buttons'''
 		self.button_frame = Frame(relief = 'raised', bg = 'gray')
 		self.button_frame.pack(fill = 'x', expand = True)
-		self.download_button = Button(self.master, relief = "raised", text = "Descargar", command = lambda: self.download_from_text())
+		self.download_button = Button(self.master, relief = 'raised', text = 'Descargar', command = lambda: self.download_from_text())
 		self.download_button.pack(fill = 'x', expand = True)
-		self.exit_button = Button(self.master, relief = "raised", text = "Salir", command = self.master.quit)
+		self.exit_button = Button(self.master, relief = 'raised', text = 'Salir', command = self.master.quit)
 		self.exit_button.pack(fill = 'x', expand = True)
 
 	def download_from_text(self):
 		'''
-		Method that downloads everything in the text file. It shows a message when the process finishes.
-		This method deletes the text file also.
-		'''
+				Method that downloads everything in the text file. It shows a message when the process finishes.
+				This method deletes the text file also.
+				'''
 		self.write_in_text()
-		os.system("youtube-dl --extract-audio --audio-format mp3 -a " + self.f_name + ' > tmp')
-		out = open('tmp', 'r').read()
-		print('---------------------------------------')
-		print(out)
-		print('---------------------------------------')
+		#https://www.youtube.com/watch?v=AXvr66tOERo&frags=pl%2Cwn
+		#https://www.youtube.com/watch?v=DMkDwIM3piA&list=PL0_Zm4Wcsi0K_KT9jkrwPYhCr-oK1vU-r
+
+		## Esta manera de ejecutar el comando sirve para capturar la salida pero no los errores
+		os.system('youtube-dl --extract-audio --audio-format mp3 -a ' + self.f_name + ' > tmp')
+		out = open('tmp', 'r').readlines()
+
+		# TODO: HAY QUE PONER ESTO BIEN! DEVUELVE NONETYPE
+		a = []
+		for line in out:
+			groups = re.search('(\[.+?\]\sDestination\:)(.+?)$', line)
+			print(type(groups))
+			print(groups)
+			a.append(groups(0))
+		print(a)
+
+		# ERROR QUE NECESITA UPDATE: Make sure you are using the latest version; type  youtube-dl -U  to update.
+		# NUMERO DE CANCIONES DESCARGADAS: Concatenacion [download] Destination: + [ffmpeg] Destination:
+
+		#try:
+		#	subprocess.call(['youtube-dl --extract-audio --audio-format mp3 -a', self.f_name])
+		#	messagebox.showinfo('Hecho', 'Se ha completado la descarga de ' + str(n_songs) + ' canciones')
+		#self.delete_file()
+		#except NameError as err: #NameError as err or
+		#	messagebox.showerror('Error', 'La dirección ' +' no es válida.' + str(err))
+		#	self.delete_file()
+		#except OSError as err:
+		#	messagebox.showerror('Error', 'Ha ocurrido un error en la ejecución \n OS error:'+ str(err) + '.')
+		#	self.delete_file()
+
+		self.delete_file()
 		os.remove('tmp')
-		#subprocess.run(["youtube-dl --extract-audio --audio-format mp3 -a", self.f_name])
 		### TODO: convertir py2exe --> Windows
 		### TODO: Hay que conseguir capturar el output de la consola para ver que pone y sacar un mensaje de error.
-		### TODO: Se puede parsear para ver si pone que hay que actualizar youtube-dl y si es asi lanzar el comando de actualizacion. Con estp sacar una ventana que diga: se esta actualizando
-		#https://www.youtube.com/watch?v=AXvr66tOERo&frags=pl%2Cwn
-		#https://www.youtube.com/watch?v=8t4O5RnLSKI&frags=pl%2Cwn
-		self.delete_file()
-		messagebox.showinfo("Hecho", "Se ha completado la descarga de " + str(self.n_songs) + " canciones.")
 
 	def write_in_text(self):
 		'''
 		Method that reads the lines in the text area and writes them in the file that is going to be used in the bash process
 		'''
 
-		input_lines = self.text_area.get("1.0", END).splitlines()
+		input_lines = self.text_area.get('1.0', END).splitlines()
 		try:  # Python 2.x
-			input_lines = [u.encode("utf-8") for u in input_lines]
+			input_lines = [u.encode('utf-8') for u in input_lines]
 			input_lines = list(filter(str.strip, input_lines))
 		except TypeError: # Python 3.x
-			input_lines = [b.decode("utf-8") for b in input_lines]
+			input_lines = [b.decode('utf-8') for b in input_lines]
 			input_lines = list(filter(str.strip, input_lines))
-		self.n_songs = len(input_lines)
+		self.file = open(self.f_name, 'w+')
 		for l in input_lines:
+			#print('------------------')
+			#print(l)
+			#print('------------------')
 			self.file.write(l+'\n')
 		self.file.close()
 
